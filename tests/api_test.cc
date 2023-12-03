b4d81f7ce1f0da3cae52b3e6c0e25d11548c5f82
Yong Seung Lee <yongseunglee@Yongs-MacBook-Air.local>
2023-12-03 18:57:54 -0500

    fix

--- tests/api_test.cc
+++ tests/api_test.cc
@@ -1912,7 +1912,7 @@ TEST(ApiTest, GhostCloneGhost) {
   constexpr int kCpuNum = 0;
   Topology* topology = MachineTopology();
 
-  auto ap = AgentProcess<FullFifoAgent<LocalEnclave>, AgentConfig>(
+  auto ap = AgentProcess<FullFifoAgent<LocalEnclave>, ghost::ProfilingAgentConfig>(
       AgentConfig(topology, topology->ToCpuList(std::vector<int>{kCpuNum})));
 
   // Verify that a ghost thread implicitly clones itself in the ghost
