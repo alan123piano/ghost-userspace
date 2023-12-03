75c65f3f7c07fba9aab75b8ab08b75946b97f31d
Alan Yang <alan123piano@gmail.com>
2023-12-03 00:25:42 -0500

    vestigal scripts

--- scripts/orca.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "build" ]; then
-    make clean -C orca
-    make -C orca
-else
-    ARGS=("$@")
-    sudo ./orca/orca ${ARGS[@]}
-fi
--- scripts/orca_client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$1" == "build" ]; then
-    make clean -C orca
-    make -C orca
-else
-    ARGS=("$@")
-    ./orca/orca_client ${ARGS[@]}
-fi
