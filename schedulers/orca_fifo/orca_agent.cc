b8b9faa6dfdd28387daa6e78237ef2418ddd5bae
Yong Seung Lee <yongseunglee@Yongs-MacBook-Air.local>
2023-12-08 19:20:08 -0500

    trying out switching

---
+++ schedulers/orca_fifo/orca_agent.cc
@@ -0,0 +1,117 @@
+// Copyright 2021 Google LLC
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+#include <cstdint>
+#include <string>
+#include <vector>
+
+#include "absl/debugging/symbolize.h"
+#include "absl/flags/parse.h"
+#include "lib/agent.h"
+#include "lib/enclave.h"
+#include "schedulers/orca_fifo/orca_scheduler.h"
+
+ABSL_FLAG(std::string, ghost_cpus, "1-5", "cpulist");
+ABSL_FLAG(int32_t, globalcpu, -1,
+          "Global cpu. If -1, then defaults to the first cpu in <cpus>");
+ABSL_FLAG(int32_t, profiler_cpu, -1,
+          "Profiler cpu. If -1, then defaults to the first cpu in <cpus>");
+ABSL_FLAG(std::string, enclave, "", "Connect to preexisting enclave directory");
+
+namespace ghost
+{
+
+    static void ParseAgentConfig(OrcaFifoAgentConfig *config)
+    {
+        CpuList ghost_cpus =
+            MachineTopology()->ParseCpuStr(absl::GetFlag(FLAGS_ghost_cpus));
+        CHECK(!ghost_cpus.Empty());
+
+        Topology *topology = MachineTopology();
+        config->topology_ = topology;
+        config->cpus_ = ghost_cpus;
+
+        int profiler_cpu = absl::GetFlag(FLAGS_profiler_cpu);
+        if (profiler_cpu < 0)
+        {
+            CHECK_EQ(profiler_cpu, -1);
+            profiler_cpu = ghost_cpus.Front().id();
+            absl::SetFlag(&FLAGS_profiler_cpu, profiler_cpu);
+        }
+        CHECK(ghost_cpus.IsSet(profiler_cpu));
+        config->profiler_cpu = profiler_cpu;
+
+        int globalcpu = absl::GetFlag(FLAGS_globalcpu);
+        if (globalcpu < 0)
+        {
+            CHECK_EQ(globalcpu, -1);
+            globalcpu = ghost_cpus.Front().id();
+            absl::SetFlag(&FLAGS_globalcpu, globalcpu);
+        }
+        CHECK(ghost_cpus.IsSet(globalcpu));
+
+        config->global_cpu_ = topology->cpu(globalcpu);
+        config->preemption_time_slice_ = absl::GetFlag(FLAGS_preemption_time_slice);
+
+        std::string enclave = absl::GetFlag(FLAGS_enclave);
+        if (!enclave.empty())
+        {
+            int fd = open(enclave.c_str(), O_PATH);
+            CHECK_GE(fd, 0);
+            config->enclave_fd_ = fd;
+        }
+    }
+} // namespace ghost
+
+int main(int argc, char *argv[])
+{
+    absl::InitializeSymbolizer(argv[0]);
+    absl::ParseCommandLine(argc, argv);
+
+    ghost::ProfilingAgentConfig config;
+    ghost::ParseAgentConfig(&config);
+
+    printf("Initializing...\n");
+
+    // Using new so we can destruct the object before printing Done
+    auto uap = new ghost::AgentProcess<ghost::FullFifoAgent<ghost::LocalEnclave>,
+                                       ghost::OrcaFifoAgentConfig>(config);
+
+    ghost::GhostHelper()->InitCore();
+    printf("Initialization complete, ghOSt active.\n");
+    // When `stdout` is directed to a terminal, it is newline-buffered. When
+    // `stdout` is directed to a non-interactive device (e.g, a Python subprocess
+    // pipe), it is fully buffered. Thus, in order for the Python script to read
+    // the initialization message as soon as it is passed to `printf`, we need to
+    // manually flush `stdout`.
+    fflush(stdout);
+
+    ghost::Notification exit;
+    ghost::GhostSignals::AddHandler(SIGINT, [&exit](int)
+                                    {
+    static bool first = true;  // We only modify the first SIGINT.
+
+    if (first) {
+      exit.Notify();
+      first = false;
+      return false;  // We'll exit on subsequent SIGTERMs.
+    }
+    return true; });
+
+    // TODO: this is racy - uap could be deleted already
+    ghost::GhostSignals::AddHandler(SIGUSR1, [uap](int)
+                                    {
+    uap->Rpc(ghost::FifoScheduler::kDebugRunqueue);
+    return false; });
+
+    exit.WaitForNotification();
+
+    delete uap;
+
+    printf("\nDone!\n");
+
+    return 0;
+}
--- schedulers/orca_fifo/orca_scheduler.h
+++ schedulers/orca_fifo/orca_scheduler.h
@@ -10,29 +10,114 @@
 
 namespace ghost
 {
+    enum class FIFOSCHEDTYPE
+    {
+        PER_CPU,
+        CENT
+    };
+
+    class OrcaFifoAgentConfig : public ProfilingAgentConfig
+    {
+    public:
+        OrcaFifoAgentConfig() {}
+
+        OrcaFifoAgentConfig(Topology *topology, CpuList cpulist, int32_t profiler_cpu_, Cpu global_cpu,
+                            absl::Duration preemption_time_slice)
+            : ProfilingAgentConfig(topology, std::move(cpulist), profiler_cpu_),
+              global_cpu_(global_cpu.id()),
+              preemption_time_slice_(preemption_time_slice) {}
+
+        OrcaFifoAgentConfig(Topology *topology, CpuList cpulist, Cpu global_cpu,
+                            absl::Duration preemption_time_slice)
+            : ProfilingAgentConfig(topology, std::move(cpulist)),
+              global_cpu_(global_cpu.id()),
+              preemption_time_slice_(preemption_time_slice) {}
+
+        int32_t global_cpu_;
+        absl::Duration preemption_time_slice_ = absl::InfiniteDuration();
+    };
 
     template <class EnclaveType>
     class FullFifoAgent : public FullAgent<EnclaveType>
     {
     public:
-        explicit FullFifoAgent(ProfilingAgentConfig config) : FullAgent<EnclaveType>(config), profiler_cpu(config.profiler_cpu)
+        explicit FullFifoAgent(OrcaFifoAgentConfig config) : FullAgent<EnclaveType>(config), profiler_cpu(config.profiler_cpu), global_cpu(config.global_cpu_),
+                                                             preemption_time_slice(config.preemption_time_slice_)
         {
-            scheduler_ =
-                MultiThreadedFifoScheduler(&this->enclave_, *this->enclave_.cpus());
-
             orcaMessenger = std::make_unique<OrcaMessenger>();
+            initPerCPU();
+        }
+        void initPerCPU()
+        {
+            currentSched = FIFOSCHEDTYPE::PER_CPU;
+            per_cpu_scheduler = per_cpu::MultiThreadedFifoScheduler(&this->enclave_, *this->enclave_.cpus());
             this->StartAgentTasks();
             this->enclave_.Ready();
+        }
+        void initCent()
+        {
+            currentSched = FIFOSCHEDTYPE::CENT;
+            centralized_scheduler = centralized::SingleThreadFifoScheduler(&this->enclave_, *this->enclave_.cpus(), this->global_cpu, this->preemption_time_slice_);
+            this->StartAgentTasks();
+            this->enclave_.Ready();
+        }
+        void switchTo(FIFOSCHEDTYPE to)
+        {
+            if (to == FIFOSCHEDTYPE::PER_CPU)
+            {
+                CHECK_EQ(currentSched, FIFOSCHEDTYPE::CENT);
+                destroyCent();
+                this->TerminateAgentTasks();
+                delete centralized_scheduler;
+                centralized_scheduler = nullptr;
+                currentSched = FIFOSCHEDTYPE::PER_CPU;
+                initPerCPU();
+            }
+            else
+            {
+                CHECK_EQ(currentSched, FIFOSCHEDTYPE::PER_CPU);
+                this->TerminateAgentTasks();
+                delete per_cpu_scheduler;
+                per_cpu_scheduler = nullptr;
+                currentSched = FIFOSCHEDTYPE::CENT;
+                initCent();
+            }
+        }
+
+        void destroyCent()
+        {
+            auto global_cpuid = centralized_scheduler->GetGlobalCPUId();
+            if (this->agents_.front()->cpu().id() != global_cpuid)
+            {
+                // Bring the current globalcpu agent to the front.
+                for (auto it = this->agents_.begin(); it != this->agents_.end(); it++)
+                {
+                    if (((*it)->cpu().id() == global_cpuid))
+                    {
+                        auto d = std::distance(this->agents_.begin(), it);
+                        std::iter_swap(this->agents_.begin(), this->agents_.begin() + d);
+                        break;
+                    }
+                }
+            }
+            CHECK_EQ(this->agents_.front()->cpu().id(), global_cpuid);
         }
 
         ~FullFifoAgent() override
         {
+            if (currentSched == FIFOSCHEDTYPE::CENT)
+            {
+                destroyCent();
+            }
             this->TerminateAgentTasks();
         }
 
         std::unique_ptr<Agent> MakeAgent(const Cpu &cpu) override
         {
-            return std::make_unique<FifoAgent>(&this->enclave_, cpu, scheduler_.get(), profiler_cpu, orcaMessenger.get());
+            if (currentSched == FIFOSCHEDTYPE::PER_CPU)
+                return std::make_unique<per_cpu::FifoAgent>(&this->enclave_, cpu, per_cpu_scheduler.get(), profiler_cpu, orcaMessenger.get());
+            else
+                return std::make_unique<centralized::FifoAgent>(&this->enclave_, cpu, centralized_scheduler.get(), orcaMessenger.get());
         }
 
         void RpcHandler(int64_t req, const AgentRpcArgs &args,
@@ -41,11 +126,11 @@ namespace ghost
             switch (req)
             {
             case FifoScheduler::kDebugRunqueue:
-                scheduler_->debug_runqueue_ = true;
+                per_cpu_scheduler->debug_runqueue_ = true;
                 response.response_code = 0;
                 return;
             case FifoScheduler::kCountAllTasks:
-                response.response_code = scheduler_->CountAllTasks();
+                response.response_code = per_cpu_scheduler->CountAllTasks();
                 return;
             default:
                 response.response_code = -1;
@@ -54,8 +139,12 @@ namespace ghost
         }
 
     private:
-        std::unique_ptr<FifoScheduler> scheduler_;
+        FIFOSCHEDTYPE currentSched;
+        std::unique_ptr<per_cpu::FifoScheduler> per_cpu_scheduler;
+        std::unique_ptr<centralized::FifoScheduler> centralized_scheduler;
         std::unique_ptr<OrcaMessenger> orcaMessenger;
         int32_t profiler_cpu;
+        int32_t global_cpu;
+        absl::Duration preemption_time_slice = absl::InfiniteDuration();
     };
 }
